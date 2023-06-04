a15a724cebb6503313a4616415fb3468e123ea04
VolodymyrSkaskiv <skaskivvolodymyr@gmail.com>
2023-06-04 16:35:54 +0300

    copy hw06

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { ContactForm } from './contactForm/ContactForm';
+import { ContactList } from './contactList/ContactList';
+import { Filter } from './filter/Filter';
+import { MainHeader, SecondaryHeader, Section } from './App.styled';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Section>
+      <MainHeader>Phonebook</MainHeader>
+      <ContactForm />
+      <SecondaryHeader>Contacts</SecondaryHeader>
+      <Filter />
+      <ContactList />
+      <ToastContainer position="top-center" /> {/* відображення сповіщень */}
+    </Section>
   );
 };
